746638c9a2fa6cb942c90706e73e6579d1cadfcd
nuntius35 <nuntius35@example.com>
2017-09-23 16:02:04 +0200

    Perform checks automatically on startup and use preference fragment

--- app/src/main/java/org/nuntius35/wrongpinshutdown/MainActivity.java
+++ app/src/main/java/org/nuntius35/wrongpinshutdown/MainActivity.java
@@ -1,21 +1,15 @@
 package org.nuntius35.wrongpinshutdown;
 
 import android.app.admin.DevicePolicyManager;
-import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.preference.PreferenceFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    static final int ACTIVATION_REQUEST = 47; // identifies our request id
+    static final int ACTIVATION_REQUEST = 1;
     final Context context = this;
 
     DevicePolicyManager devicePolicyManager;
@@ -23,75 +17,35 @@ public class MainActivity extends AppCompatActivity {
 
     ShutdownManager sm;
 
-    protected Button adminButton;
-    protected Button testButton;
-    protected Button settingButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
-        sm = new ShutdownManager(context);
 
-        adminButton = (Button) findViewById(R.id.adminButton);
-        testButton = (Button) findViewById(R.id.testButton);
-        settingButton = (Button) findViewById(R.id.settingButton);
+        sm = new ShutdownManager(context);
 
         devicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
         demoDeviceAdmin = new ComponentName(this, LogInReceiver.class);
 
-        adminButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                boolean isActive = devicePolicyManager.isAdminActive(demoDeviceAdmin);
-                if (!isActive) {
-                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, demoDeviceAdmin);
-                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
-                            getString(R.string.adminExplanation));
-                    startActivityForResult(intent, ACTIVATION_REQUEST);
-                }
-                else {
-                    Toast.makeText(MainActivity.this,
-                    R.string.isAdmin, Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        boolean isActive = devicePolicyManager.isAdminActive(demoDeviceAdmin);
+        if (!isActive) {
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, demoDeviceAdmin);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                    getString(R.string.adminExplanation));
+            startActivityForResult(intent, ACTIVATION_REQUEST);
+        }
 
-        testButton.setOnClickListener(new View.OnClickListener() {
-           public void onClick(View v) {
-               AlertDialog.Builder alert = new AlertDialog.Builder(context);
-               TextView t_view = new TextView(context);
-               t_view.setText(R.string.confirmReboot);
-               alert.setCustomTitle(t_view);
-               alert.setMessage(R.string.helpReboot);
-               alert.setIcon(android.R.drawable.ic_dialog_alert);
-               alert.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int whichButton) {
-                       sm.shutdownDevice();
-                   }});
-               alert.setNegativeButton(android.R.string.no, null);
-               alert.show();
-           }
-        });
+        sm.testRoot();
+    }
 
-        settingButton.setOnClickListener(new View.OnClickListener() {
-           public void onClick(View v) {
-               AlertDialog.Builder alert = new AlertDialog.Builder(context);
-               TextView t_view = new TextView(context);
-               t_view.setText(R.string.settingDialog);
-               alert.setCustomTitle(t_view);
-               String[] numbers = {"1", "2", "3", "4", "5"};
-               int checkedItem = sm.getMaxTries();
-               alert.setSingleChoiceItems(numbers, checkedItem,
-                   new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialog, int which) {
-                         sm.setMaxTries(which);
-                     }
-               });
-               alert.setPositiveButton(android.R.string.ok, null);
-               alert.show();
-           }
-        });
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
--- app/src/main/java/org/nuntius35/wrongpinshutdown/ShutdownManager.java
+++ app/src/main/java/org/nuntius35/wrongpinshutdown/ShutdownManager.java
@@ -2,30 +2,20 @@ package org.nuntius35.wrongpinshutdown;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 class ShutdownManager {
     private static final String TRIES_KEY = "tries";
     private static final String MAX_TRIES_KEY = "max_tries";
-    private static final String PREFERENCES_KEY = "org.nuntius35.wrongpinshutdown.prefs";
 
     private int tries;
     private int max_tries;
     private SharedPreferences sharedPref;
 
     ShutdownManager(Context context) {
-        sharedPref = context.getSharedPreferences(
-		        PREFERENCES_KEY, Context.MODE_PRIVATE);
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
         tries = sharedPref.getInt(TRIES_KEY, 0);
-        max_tries = sharedPref.getInt(MAX_TRIES_KEY, 2);
-    }
-
-    int getMaxTries() {
-        return max_tries;
-    }
-
-    void setMaxTries(int m) {
-        max_tries = m;
-        sharedPref.edit().putInt(MAX_TRIES_KEY, m).apply();
+        max_tries = Integer.parseInt(sharedPref.getString(MAX_TRIES_KEY, "2"));
     }
 
     void resetTries() {
@@ -36,16 +26,26 @@ class ShutdownManager {
     void incTries() {
         tries++;
         sharedPref.edit().putInt(TRIES_KEY, tries).apply();
+        max_tries = Integer.parseInt(sharedPref.getString(MAX_TRIES_KEY, "2"));
         if (tries > max_tries) {
 			resetTries();
 			shutdownDevice();
 		}
     }
 
-    void shutdownDevice() {
+    private void shutdownDevice() {
         try {
             Process process = Runtime.getRuntime().exec(
 			new String[] { "su", "-c", "reboot", "-p" });
+			process.waitFor();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    void testRoot() {
+        try {
+            Process process = Runtime.getRuntime().exec(
+			new String[] {"su", "-c", "ls"});
 			process.waitFor();
         } catch (Exception ex) {
             ex.printStackTrace();
