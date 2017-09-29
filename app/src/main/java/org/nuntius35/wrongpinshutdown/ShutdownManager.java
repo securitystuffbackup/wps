b6449f74ca0406484745769f359026ea31da24d9
nuntius35 <nuntius35@example.com>
2017-09-29 21:36:45 +0200

    Add option to edit shutdown command.

--- app/src/main/java/org/nuntius35/wrongpinshutdown/ShutdownManager.java
+++ app/src/main/java/org/nuntius35/wrongpinshutdown/ShutdownManager.java
@@ -7,15 +7,18 @@ import android.preference.PreferenceManager;
 class ShutdownManager {
     private static final String TRIES_KEY = "tries";
     private static final String MAX_TRIES_KEY = "max_tries";
+    private static final String SHUTDOWN_KEY = "shutdown_cmd";
 
     private int tries;
     private int max_tries;
     private SharedPreferences sharedPref;
+    private String shutdown_cmd;
 
     ShutdownManager(Context context) {
         sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
         tries = sharedPref.getInt(TRIES_KEY, 0);
         max_tries = Integer.parseInt(sharedPref.getString(MAX_TRIES_KEY, "2"));
+        shutdown_cmd = sharedPref.getString(SHUTDOWN_KEY,null);
     }
 
     void resetTries() {
@@ -36,7 +39,7 @@ class ShutdownManager {
     private void shutdownDevice() {
         try {
             Process process = Runtime.getRuntime().exec(
-			new String[] { "su", "-c", "reboot -p" });
+			new String[] { "su", "-c", shutdown_cmd});
 			process.waitFor();
         } catch (Exception ex) {
             ex.printStackTrace();
