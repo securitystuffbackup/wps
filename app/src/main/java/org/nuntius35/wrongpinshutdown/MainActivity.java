ce761bc0e1f4605a8556540db9cab7b95a56b7f3
nuntius35 <nuntius35@example.com>
2017-11-25 17:43:08 +0100

    Echo shutdown command in preference screen

--- app/src/main/java/org/nuntius35/wrongpinshutdown/MainActivity.java
+++ app/src/main/java/org/nuntius35/wrongpinshutdown/MainActivity.java
@@ -4,9 +4,13 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.Preference;
+import android.preference.EditTextPreference;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     static final int ACTIVATION_REQUEST = 1;
@@ -40,12 +44,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static class MyPreferenceFragment extends PreferenceFragment
-    {
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
         @Override
-        public void onCreate(final Bundle savedInstanceState)
-        {
+        public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            onSharedPreferenceChanged(sharedPrefs, "shutdown_cmd");
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
+
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
+
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Preference pref = findPreference(key);
+            if (pref instanceof EditTextPreference) {
+                EditTextPreference listPref = (EditTextPreference) pref;
+                pref.setSummary(listPref.getText());
+            }
         }
     }
 }
