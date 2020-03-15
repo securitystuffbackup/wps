980dba19569802180a13967c92f1b90110ace97e
Andreas Geyer-Schulz <gsa@posteo.de>
2020-03-15 16:30:07 +0100

    Update UserReceiver: check intent with a filter

--- app/src/main/java/org/nuntius35/wrongpinshutdown/UserReceiver.java
+++ app/src/main/java/org/nuntius35/wrongpinshutdown/UserReceiver.java
@@ -3,12 +3,14 @@ package org.nuntius35.wrongpinshutdown;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 
 public class UserReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         ShutdownManager sm = new ShutdownManager(context);
-        if (intent.getAction().equals(Intent.ACTION_USER_PRESENT)) {
+        IntentFilter filter = new IntentFilter(Intent.ACTION_USER_PRESENT);
+        if (filter.matchAction(intent.getAction())) {
             sm.resetTries();
         }
     }
